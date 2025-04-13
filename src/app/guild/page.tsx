4c1932c33ee5ce8e2292c68d5fd7209d4750f4e1
openmpy <suhwan@kakao.com>
2025-04-13 20:37:58 +0900

    feat: 페이지 추가

---
+++ src/app/guild/page.tsx
@@ -0,0 +1,7 @@
+export default function Runner() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">길드 사전</h1>
+    </div>
+  );
+}
---
+++ src/app/runner/page.tsx
@@ -0,0 +1,7 @@
+export default function Runner() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">런너 사전</h1>
+    </div>
+  );
+}
