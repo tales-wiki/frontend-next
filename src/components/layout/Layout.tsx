33c021465db85a9d8f08de83d1cbb7fa2dce0ec5
openmpy <suhwan@kakao.com>
2025-04-13 15:37:10 +0900

    feat: 폴더 구조 작성

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Footer from "./Footer";
+import Header from "./Header";
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout = ({ children }: LayoutProps) => {
+  return (
+    <div>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
