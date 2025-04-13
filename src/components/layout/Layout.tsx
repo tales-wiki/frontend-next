5fb26ac8b2fd613356a3f011913664d7ed77d46a
openmpy <suhwan@kakao.com>
2025-04-13 17:32:21 +0900

    style: 레이아웃 디자인 수정

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -11,7 +11,7 @@ const Layout = ({ children }: LayoutProps) => {
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
-      <main className="flex-1 grid grid-cols-1 lg:grid-cols-[8.5fr_1.5fr] gap-4 px-0 lg:px-4 py-4">
+      <main className="flex-1 grid grid-cols-1 lg:grid-cols-[1fr_250px] gap-4 px-0 lg:px-4 py-4">
         <div className="bg-white p-4 border border-slate-400 lg:rounded-lg">
           {children}
         </div>
